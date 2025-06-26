649eff2432e01dac95396c2a0084a85a71d27366
Mohaned <akumaxd5@gmail.com>
2025-06-26 15:41:57 +0100

    chore: separate logical implementation group

--- src/components/01 - Home/NoteArea.tsx
+++ src/components/01 - Home/NoteArea.tsx
@@ -1,4 +1,4 @@
-import { createEffect } from "solid-js";
+import { createEffect, createSignal } from "solid-js";
 import { useKatex } from "@/utils/katex_support";
 import { useMarkdown } from "@/utils/markdown_support";
 
@@ -12,20 +12,34 @@ export default function TextDisplayArea(props: Props) {
   const { renderInElement } = useKatex();
   const { parse } = useMarkdown();
 
+  const [accumulatedText, setAccumulatedText] = createSignal("");
+
+  createEffect(() => {
+    if (props.text) {
+        setAccumulatedText(prev => prev + props.text);
+    }
+  });
+
   createEffect(async () => {
     if (!textContainerRef) return;
-    
-    // Parse markdown first (which preserves LaTeX as script tags)
-    const htmlContent = await parse(props.text);
-    textContainerRef.innerHTML = htmlContent;
-    
-    // Then render LaTeX/KaTeX from the script tags and any remaining LaTeX
-    renderInElement(textContainerRef);
+
+    const currentFullText = accumulatedText();
+
+    console.log("String being sent to parse (accumulated):", currentFullText);
+
+    if (currentFullText) {
+      const htmlContent = await parse(currentFullText);
+      textContainerRef.innerHTML = htmlContent;
+
+      renderInElement(textContainerRef);
+    } else {
+      textContainerRef.innerHTML = '';
+    }
   });
 
   return (
     <div
-      class={`p-4 prose prose-sm max-w-none dark:prose-invert whitespace-pre-wrap ${props.class || ""}`}
+      class={`p-4 prose prose-sm max-w-full dark:prose-invert select-all whitespace-pre-wrap ${props.class || ""}`}
       id="text"
       ref={textContainerRef}
       contentEditable={false}

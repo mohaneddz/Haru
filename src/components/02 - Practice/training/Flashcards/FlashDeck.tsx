211197420c3562a6e0f6b17ef4dec07958e85338
Mohaned <akumaxd5@gmail.com>
2025-07-08 12:51:07 +0100

    chore: separate logical implementation group

---
+++ src/components/02 - Practice/training/Flashcards/FlashDeck.tsx
@@ -0,0 +1,28 @@
+import LayoutCard from '@/components/02 - Practice/training/LayoutCard';
+import { Trash2 } from 'lucide-solid';
+
+interface Props {
+    title: string;
+    description?: string;
+    id?: number;
+    class?: string;
+}
+
+export default function FlashDeck(props: Props) {
+    return (
+        <LayoutCard hoverable={false} class="p-6 border rounded-2xl shadow-xl bg-background-dark-1 text-white flex flex-col gap-4">
+
+            <p class="text-lg text-accent-light-1 truncate font-bold" value-id={props.id}>{props.title}
+                <Trash2 class="inline-block ml-2 text-gray-400 hover:text-error transition-colors cursor-pointer" />
+            </p>
+
+            <p class="text-xs opacity-80 truncate text-left w-[80%]">{props.description}</p>
+
+            <div class="flex gap-2 w-full">
+                <a href={`/practice/training/flashcards/${props.id}`} class="text-center bg-accent w-full hover:scale-103 hover:bg-accent-dark-1 transition-all duration-75 hover:cursor-pointer p-1 rounded">Start</a>
+                <a href={`/practice/training/flashcards/${props.id}`} class="text-center bg-sidebar-light-3 w-full hover:scale-103 hover:bg-sidebar-light-1 transition-all duration-75 hover:cursor-pointer p-1 rounded">Edit</a>
+            </div>
+
+        </LayoutCard>
+    );
+}
--- src/components/02 - Practice/training/LayoutCard.tsx
+++ src/components/02 - Practice/training/LayoutCard.tsx
@@ -6,6 +6,7 @@ interface TrainingCardProps {
   hoverable?: boolean;
   class?: string;
   border?: boolean;
+  href?: string;
 }
 
 export default function LayoutCard(props: TrainingCardProps) {
@@ -21,31 +22,33 @@ export default function LayoutCard(props: TrainingCardProps) {
   };
 
   const hoverClasses = (props.hoverable ?? true)
-    ? 'hover:scale-95 active:scale-90 hover:from-gray-700 hover:to-gray-800'
+    ? 'hover:scale-95 active:scale-90 hover:from-gray-700 hover:to-gray-800 hover:cursor-pointer'
     : '';
 
   const borderHoverClasses = (props.hoverable ?? true)
-    ? 'hover:shadow-lg transition duration-100 hover:scale-103 hover:cursor-pointer active:scale-100'
+    ? 'hover:shadow-lg transition duration-100 hover:scale-103 active:scale-100'
     : 'transition duration-100';
+
+  const href = props.href ? `${props.href}` : undefined;
 
   return (
     <>
       {props.border ? (
-        <div class={`bg-gradient-to-br from-border-light-2 to-border-dark-2 rounded-lg shadow-md overflow-hidden ${borderHoverClasses} `}>
-          <div class="p-0.5 h-full">
+        <a href={href} class={`bg-gradient-to-br from-border-light-2 to-border-dark-2 rounded-lg shadow-md overflow-hidden ${borderHoverClasses} block w-full h-full`}>
+          <div class="p-0.5 h-full w-full">
             <div
-              class={`h-full w-full flex flex-col items-center justify-center px-4 py-8 bg-gradient-to-b from-gray-800 to-gray-900 rounded-md cursor-pointer transition-all duration-300 ${props.class || ''} ${getVariationClasses()}`}
+              class={`h-full w-full flex flex-col items-center justify-center px-4 py-8 bg-gradient-to-b from-gray-800 to-gray-900 rounded-md transition-all duration-300 ${props.class || ''} ${getVariationClasses()}`}
             >
               {props.children}
             </div>
           </div>
-        </div>
+        </a>
       ) : (
-        <div
-          class={`flex flex-col items-center justify-center px-4 py-8 bg-gradient-to-b from-gray-800 to-gray-900 rounded-lg cursor-pointer transition-all duration-300 ${props.class || ''} ${hoverClasses} ${getVariationClasses()}`}
+        <a href={href}
+          class={`flex flex-col items-center justify-center px-4 py-8 bg-gradient-to-b from-gray-800 to-gray-900 rounded-lg transition-all duration-300 ${props.class || ''} ${hoverClasses} ${getVariationClasses()} block w-full h-full`}
         >
           {props.children}
-        </div>
+        </a>
       )}
     </>
   );

8f9a16e85943f114c88b89d191cd7b8293ac9b2b
Mohaned <akumaxd5@gmail.com>
2025-07-26 22:17:03 +0100

    chore: separate logical implementation group

--- src/routes/01 - Home/Notes.tsx
+++ src/routes/01 - Home/Notes.tsx
@@ -5,21 +5,21 @@ import NoteTopBar from "@/components/01 - Home/Notes/NoteTopBar";
 import NotesToolBar from "@/components/01 - Home/Notes/NotesToolBar";
 import { TextExample } from "@/data/TextExample";
 
-import { Paperclip } from "lucide-solid";
+// import { Paperclip } from "lucide-solid";
 
 
-function FileSelectorButton() {
-  return (
-    <a class="absolute bottom-4 left-0 right-0 mx-auto w-fit h-10 bg-background-dark-2 border border-border rounded-md flex items-center justify-center text-muted hover:bg-muted/20 transition-colors duration-200 cursor-pointer z-50">
-      // <Paperclip size={18} class="mr-2" />
-    </a>
-  );
-};
+// function FileSelectorButton() {
+//   return (
+//     <a class="absolute bottom-4 left-0 right-0 mx-auto w-fit h-10  border border-border rounded-md flex items-center justify-center text-muted hover:bg-muted/20 transition-colors duration-200 cursor-pointer z-50">
+//       // <Paperclip size={18} class="mr-2" />
+//     </a>
+//   );
+// };
 
 export default function Notes() {
 
   return (
-    <div class="flex items-center justify-start h-full w-full bg-background-dark-2">
+    <div class="flex items-center justify-start h-full w-full ">
 
       <Files class='z-50' />
 
